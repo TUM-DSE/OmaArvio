c262eac4ccdc63da8809345135439b8cc1722932
Thore Sommer <thore.sommer@tum.de>
2026-06-02 13:47:01 +0200

    nix: update nixpkgs version
    we need to pin the version for openssl at the moment

--- modules/crypto/flake.nix
+++ modules/crypto/flake.nix
@@ -3,23 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Pinned separately because our custom openssl-aegis branch does not build
+    # against newer nixpkgs.
+    nixpkgs-openssl.url = "github:NixOS/nixpkgs/2fad6eac6077f03fe109c4d4eb171cf96791faa4";
     core = {
       url = "path:../../core";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, core }:
+  outputs = { self, nixpkgs, nixpkgs-openssl, core }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
+      pkgs-openssl = import nixpkgs-openssl {
+        inherit system;
+      };
     in
     {
       packages.${system} = {
-        openssl-aegis = pkgs.callPackage ./nix/openssl-aegis.nix { };
+        openssl-aegis = pkgs-openssl.callPackage ./nix/openssl-aegis.nix { };
       };
 
       nixosModules.guest = import ./nix/guest.nix { inherit self; };
